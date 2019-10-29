08413418db7c5279337d63b588f5027cf39fcc32
MariethPerez <mperezzevallos@gmail.com>
2019-10-28 21:24:02 -0500

    Uncomment before_action and authorize for users and dialings controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 
 class UsersController < ApiController
   #Uncoment TO-DO authorize_login AND authorize
-  # before_action :authorize_login, only: [:index,:show, :create, :new]
+  before_action :authorize_login, only: [:index,:show, :create, :new, :metrics_entry_exit]
   def new
     @user = User.new
   end
 
   def index
     @users = User.all
-    # authorize [@users]
+    authorize [@users]
     render json: @users
   end
 
   def show
     @user = User.find(params[:id])
-    # authorize [@user]
+    authorize [@user]
     render json: @user
   end
 
   def create
     @user = User.new(user_params)
-    # authorize [@user]
+    authorize [@user]
     # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
     @user.email.downcase!
 
@@ -42,7 +42,7 @@ class UsersController < ApiController
     # http://localhost:3000/api/metrics_entry_exit/2/?start_date=2019-10-01&end_date=2019-12-31
      @dialings = EmployeeQueries.new.entry_exit(params[:id],params[:start_date],params[:end_date])
      @user=User.find(params[:id])
-    #  authorize [@user]
+     authorize [@user]
      render json: @dialings, status: :ok
   end
 
